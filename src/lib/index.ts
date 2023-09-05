499d87686c5485fd61fb7065a6a4744415730e79
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-09-05 18:36:11 +0300

    fix: removed duplicated util fn

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,22 +1,2 @@
 export * from "./fetch";
 export * from "./pagination";
-
-export const fetchJsonData = async <D = unknown>(
-  url: string,
-  options?: RequestInit,
-): Promise<[D, null] | [null, string]> => {
-  try {
-    const res = await fetch(url, options);
-    if (!res.ok) {
-      throw new Error(res.statusText || "Request failed");
-    }
-
-    const data: D = await res.json();
-    return [data, null];
-  } catch (error) {
-    const msg: string =
-      error instanceof Error ? error.message : (error as any).ToString();
-
-    return [null, msg];
-  }
-};
